b4f5af683767151fcea51686ddd71a4c7fc028a3
Carden Bagwell <cardenbag@gmail.com>
2016-06-26 15:59:26 -0400

    Added logging statements and fixed an issue with removing the test image.

--- python/face_detect.py
+++ python/face_detect.py
@@ -36,10 +36,11 @@ def load_test_img(test_filename='test/test_img.png', clear_test_img=False):
     dirname = os.path.dirname(os.path.realpath(__file__))
     img_filepath = os.path.join(dirname, test_filename)
     if os.path.exists(img_filepath) and not clear_test_img:
+        logger.info('Loading existing test image: {}'.format(img_filepath))
         return cv2.imread(test_filename)
     elif os.path.exists(img_filepath) and clear_test_img:
         logger.info('Clearing test file at {}'.format(img_filepath))
-        os.remove(test_filename)
+        os.remove(img_filepath)
 
     frame = capture_webcam_img()
     logger.info('Writing img to file at {}'.format(img_filepath))
@@ -80,6 +81,7 @@ def main(argv=sys.argv[1:]):
     eye_cascade = cv2.CascadeClassifier(os.path.join(CASCADE_DIR, 'haarcascade_eye.xml'))
 
     img = load_test_img(clear_test_img=args.clear_test_img)
+    print img.shape
     detect_img = detect_face(img, face_cascade)
     cv2.imshow('test', detect_img)
     cv2.waitKey(0)
